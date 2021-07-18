313cee0259641610df1cfa8656f84b3bbe766b67
Alok Mathur <73957024+alok27a@users.noreply.github.com>
2021-07-18 23:20:18 +0530

    Initial commit

---
+++ app/src/main/java/com/alok/dialogbox/MainActivity.java
@@ -0,0 +1,48 @@
+package com.alok.dialogbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Dialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.custom_layout);
+        dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.background));
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false);
+
+        Button ok = dialog.findViewById(R.id.button_okay);
+        Button cancel = dialog.findViewById(R.id.button_cancel);
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Okay", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+    }
+
+    public void onClick(View view) {
+        dialog.show();
+    }
+}
